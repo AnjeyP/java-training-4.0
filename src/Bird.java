50b4646c6f2e378cffad9776ecc561a72695872f
andrii.parfeniuk <andrii.parfeniuk@ringteam.com>
2024-10-27 03:55:31 +0200

    hw6 done

--- src/Bird.java
+++ src/Bird.java
@@ -1,7 +1,9 @@
-public class Bird {
+public abstract class Bird {
     // TODO fix class declaration
 
     public void eat() {
         // TODO print a message that bird is eating
+        System.out.println("Bird is eating");
+
     }
 }
